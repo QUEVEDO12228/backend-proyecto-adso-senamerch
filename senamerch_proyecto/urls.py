d9e7eabcf813b6ca8390ff52b8377ad41f211917
Emanuel <XxEmmaTheCrackxX@gmail.com>
2026-02-03 01:01:37 -0500

    Feat Emanuel: Se creo el login y se agregaron nuevos componentes

--- senamerch_proyecto/urls.py
+++ senamerch_proyecto/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('login')
 
 urlpatterns = [
+    path('', home, name='home'),   # 🔑 AQUÍ ESTABA EL ERROR
     path('admin/', admin.site.urls),
+    path('', include('usuarios.urls')),
 ]
