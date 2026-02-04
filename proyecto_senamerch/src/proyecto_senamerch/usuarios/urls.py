bca6eecae49f7a2e06082e724bf4334440b2af54
GitMg0 <miguelgrajales9801@gmail.com>
2026-02-04 18:36:15 -0500

    Feat Emanuel: Se agregaron nuevas funcionalidades

--- proyecto_senamerch/src/proyecto_senamerch/usuarios/urls.py
+++ proyecto_senamerch/src/proyecto_senamerch/usuarios/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
 from . import views
+from usuarios.views import home_view
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    
-    path('forgot-password/', views.forgot_password_view, name='forgot_password'),
-    path('code-verify/', views.code_verify_view, name='code_verify'),
-    path('reset-password/', views.reset_password_view, name='reset_password'),
-    path('contact/', views.contact_view, name='contact'),
 
+    # 👉 PRIMERA RUTA = HOME
+    path('', home_view, name='home'),
+
+    # LOGIN
+    path('login/', views.login_view, name='login'),
+
+    # REGISTRO
+    path('register/', views.register_view, name='register'),
+    path('register2/', views.register_step_2, name='register2'),
+
+    # ADDRESS
+    path('add-address/', views.add_address_view, name='add_address'),
+    path('add-address-2/', views.add_address_step_2, name='add_address2'),
+
+    # OTROS
+    path('contact/', views.contact_view, name='contact'),
+    
 ]
+
+    
