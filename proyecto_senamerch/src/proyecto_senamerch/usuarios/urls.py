b4ea66aefe04c4c6d615ea9ee653e67b7115f566
Juan José <jujocampore154@gmail.com>
2026-02-04 15:07:14 -0500

    featOcampo: Se realizan comentarios para que la lectura del codigo sea mas entendible

--- proyecto_senamerch/src/proyecto_senamerch/usuarios/urls.py
+++ proyecto_senamerch/src/proyecto_senamerch/usuarios/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('register/step-2/', views.register_step_2, name='register_step_2'),
+    
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('code-verify/', views.code_verify_view, name='code_verify'),
     path('reset-password/', views.reset_password_view, name='reset_password'),
