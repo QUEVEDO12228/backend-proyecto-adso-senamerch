5ccdd6ee92d1dc27ad7bf092a6e6aaa22cde0ba2
GitMg0 <miguelgrajales9801@gmail.com>
2026-02-05 15:35:13 -0500

    Feat Emanuel: Nuevas funcionalidades

--- proyecto_senamerch/src/proyecto_senamerch/usuarios/urls.py
+++ proyecto_senamerch/src/proyecto_senamerch/usuarios/urls.py
@@ -3,24 +3,19 @@ from . import views
 from usuarios.views import home_view
 
 urlpatterns = [
-
-    # 👉 PRIMERA RUTA = HOME
     path('', home_view, name='home'),
 
-    # LOGIN
     path('login/', views.login_view, name='login'),
 
-    # REGISTRO
     path('register/', views.register_view, name='register'),
     path('register2/', views.register_step_2, name='register2'),
 
-    # ADDRESS
     path('add-address/', views.add_address_view, name='add_address'),
     path('add-address-2/', views.add_address_step_2, name='add_address2'),
 
-    # OTROS
     path('contact/', views.contact_view, name='contact'),
-    
-]
 
-    
+    # 🔑 RECUPERACIÓN DE CONTRASEÑA
+    path('forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('verify-code/', views.code_verify_view, name='code_verify'),
+]
