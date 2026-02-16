22c331e4269bf6494edb1a0672dfdd8ff58c9486
Juan José <jujocampore154@gmail.com>
2026-02-16 18:07:35 -0500

    FixOcampo: Falta agregar direccion para poder continuar

--- proyecto_senamerch/src/proyecto_senamerch/tiendas/models.py
+++ proyecto_senamerch/src/proyecto_senamerch/tiendas/models.py
@@ -2,10 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tienda(models.Model):
-    propietario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tiendas')
+
+    propietario = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tiendas'
+    )
+
     nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+    categoria = models.CharField(max_length=50)
+
     descripcion = models.TextField(blank=True)
+
+    imagen_portada = models.ImageField(
+        upload_to='tiendas/',
+        blank=True,
+        null=True
+    )
+
     creada_en = models.DateTimeField(auto_now_add=True)
+
     activa = models.BooleanField(default=True)
 
     def __str__(self):
