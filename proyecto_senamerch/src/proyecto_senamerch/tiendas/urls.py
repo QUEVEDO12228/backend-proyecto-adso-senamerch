5857968aceb42339dc443f5ae7c2da6f1ebc684e
Emanuel <XxEmmaTheCrackxX@gmail.com>
2026-02-23 22:04:12 -0500

    Feat Emanuel: Nuevas interfacez de catalogo de productos del vendedor en la app de tienda

--- proyecto_senamerch/src/proyecto_senamerch/tiendas/urls.py
+++ proyecto_senamerch/src/proyecto_senamerch/tiendas/urls.py
@@ -7,6 +7,7 @@ app_name = "tiendas"
 
 urlpatterns = [
     path('home-seller/', views.home_seller, name='home_seller'),
+
     # =================================
     # CREAR TIENDA
     # =================================
@@ -18,15 +19,24 @@ urlpatterns = [
     # =================================
     path('store/add-address/', views.add_address_store, name='add_address_store'),
     path('store/add-address/step-2/', views.add_address_store2, name='add_address_store2'),
+
     path(
         'perfil-vendedor/',
         views.profile_store_seller,
         name='profile_store_seller'
     ),
+
     path('orders/', views.store_orders, name='store_orders'),
 
+    # 👉 CATÁLOGO (ESTA RUTA ESTÁ BIEN)
     path('catalogo/', views.seller_catalog, name='seller_catalog'),
-    path('producto/<int:id>/', views.view_description_product_seller, name='view_description_product_seller'),
+
+    path(
+        'producto/<int:id>/',
+        views.view_description_product_seller,
+        name='view_description_product_seller'
+    ),
+
     # =================================
     # EDITAR PERFIL VENDEDOR
     # =================================
@@ -56,6 +66,7 @@ urlpatterns = [
         views.edit_store_seller2,
         name='edit_store_seller2'
     ),
+
     path('direccion-tienda/', views.store_address_view, name='store_address'),
     path('direccion-vendedor/', views.seller_address_view, name='seller_address'),
 ]
