a0e2388b98c4e98837d6d401159d8d1eea317aed
Juan José <jujocampore154@gmail.com>
2026-03-16 17:58:51 -0500

    Feat Ocampo: Contacto

--- proyecto_senamerch/src/proyecto_senamerch/senamerch_proyecto/settings.py
+++ proyecto_senamerch/src/proyecto_senamerch/senamerch_proyecto/settings.py
@@ -13,7 +13,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
